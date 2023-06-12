6bf1e1cee4679688b6368fb3925dbaff77862b96
SerhiiRodin <rodinserj@ukr.net>
2023-06-12 23:59:50 +0300

    move DB_HOST to process.env

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 const app = require("./app");
-const {DB_HOST} = require("./config")
+// const { DB_HOST } = require("./config")
+// Чтоб видить переменную на GITе и Render.com
+const { DB_HOST } = process.env;
+
 
 mongoose.set("strictQuery", true);
 
